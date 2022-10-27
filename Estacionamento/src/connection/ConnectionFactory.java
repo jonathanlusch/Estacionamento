3b5b993f73b0378ee8af8d7085234a822d8e4e16
04928683013 <04928683013@LAB205-06.fw.iffar.edu.br>
2022-10-27 08:50:49 -0300

    semana 4 - Conlusão CRUD Vaga

--- Estacionamento/src/connection/ConnectionFactory.java
+++ Estacionamento/src/connection/ConnectionFactory.java
@@ -11,7 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class ConnectionFactory {
-    private static final String DRIVER = "com.msyql.jdbc.Driver";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3307/estacionamento";
     private static final String USER = "root";
     private static final String PASS = "";
